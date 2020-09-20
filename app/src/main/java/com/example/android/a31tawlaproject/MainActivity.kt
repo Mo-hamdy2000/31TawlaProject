86c8d97c6444f81989ffd1e6930f0c9a43e75c7a
Mo'men Hamdy <53626982+Mo-hamdy2000@users.noreply.github.com>
2020-09-20 15:31:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/a31tawlaproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.a31tawlaproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game_fragment)
+    }
+}
